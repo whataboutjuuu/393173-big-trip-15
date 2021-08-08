bcef92359850d7e440ca670659fd77f3fda9236f
Juuu <mailtojuuu@gmail.com>
2021-08-08 15:36:33 +0300

    Добавляет событие закрытия формы

--- src/view/point-popup.js
+++ src/view/point-popup.js
@@ -160,7 +160,8 @@ const createPointPopupTemplate = (point = {}) => {
         </div>
 
         <button class="event__save-btn  btn  btn--blue" type="submit">Save</button>
-        <button class="event__reset-btn" type="reset">Cancel</button>
+        <button class="event__reset-btn" type="reset">Delete</button>
+        <button class="event__rollup-btn" type="button"><span class="visually-hidden">Open event</span></button>
       </header>
       <section class="event__details">
         ${createOffersTemplate(type, point)}
